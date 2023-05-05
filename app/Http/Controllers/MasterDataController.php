4c1e5e0138d3e88d28471491899a1bc9c6e0691c
librankaio <95515543+librankaio@users.noreply.github.com>
2023-05-05 10:53:58 +0700

    update validation master data, inserting data

--- app/Http/Controllers/MasterDataController.php
+++ app/Http/Controllers/MasterDataController.php
@@ -19,11 +19,16 @@ class MasterDataController extends Controller
 
         if($request->uploadnpwp != null){
             $request->file('uploadnpwp')->store('npwp');
+            User::create(['username' => $request->username, 'name' => $request->nama, 'nik' => $request->nik, 'password' => $password, 'privilage' => $request->privilage, 'stat'=>1]);
+
+            DB::table('memp')->insert(['nik'=> $request->nik,'name'=>$request->nama,'phn'=>$request->phone,'addr'=>$request->domisili,'dob'=>$request->tanggallahir,'sex'=>$request->jeniskelamin, 'npwp'=>$request->descnpwp, 'npwp_pic'=>$request->file('uploadnpwp')->hashname(), 'username'=>$request->username, 'pwd'=>$password, 'usin'=> 1, 'stat'=> 1]);
+
+            return redirect()->back();
         } 
 
         User::create(['username' => $request->username, 'name' => $request->nama, 'nik' => $request->nik, 'password' => $password, 'privilage' => $request->privilage, 'stat'=>1]);
 
-        DB::table('memp')->insert(['nik'=> $request->nik,'name'=>$request->nama,'phn'=>$request->phone,'addr'=>$request->domisili,'dob'=>$request->tanggallahir,'sex'=>$request->jeniskelamin, 'npwp'=>$request->descnpwp, 'npwp_pic'=>$request->file('uploadnpwp')->hashname(), 'username'=>$request->username, 'pwd'=>$password, 'usin'=> 1, 'stat'=> 1]);
+        DB::table('memp')->insert(['nik'=> $request->nik,'name'=>$request->nama,'phn'=>$request->phone,'addr'=>$request->domisili,'dob'=>$request->tanggallahir,'sex'=>$request->jeniskelamin, 'npwp'=>$request->descnpwp, 'username'=>$request->username, 'pwd'=>$password, 'usin'=> 1, 'stat'=> 1]);
 
         return redirect()->back();
     }
